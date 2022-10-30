08101105dffb5cf306dbd19fb1fe54b3a3ae5695
Lev Kochetkov <108297418+KLL00-1@users.noreply.github.com>
2022-10-30 16:51:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const yes = document.getElementById("yes");
 const submit = document.getElementById("submit");
 const right = document.getElementById("right");
+const text = document.getElementById("text");
 
 submit.onclick = () => {
   if (yes.checked) {
     right.classList.toggle("container");
+    text.value = "yes";
   } else {
-    window.alert("false");
+    right.className = "right";
+    text.value = "no";
   }
 };
